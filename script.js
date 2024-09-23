1ff6afccce875e9dfdeaa8f39a27eabbded55625
ChatDuChaos <floynn1@gmail.com>
2024-09-23 03:25:12 +0200

    no message

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
-const twitchLogo = document.querySelector('.social-icons a[href*="twitch"]');
-const twitchEmbed = document.getElementById('twitch-embed');
+const amazonLogo = document.querySelector('.social-icons img[src="https://github.com/chatduchaos/ChatDuChaos/blob/main/images/Logo%20Amazon.png?raw=true"]');
+const tooltip = document.getElementById('amazon-tooltip');
 
-twitchLogo.addEventListener('mouseenter', () => {
-    twitchEmbed.style.display = 'block';
+amazonLogo.addEventListener('mouseenter', () => {
+    tooltip.style.display = 'block';
 });
 
-twitchLogo.addEventListener('mouseleave', () => {
-    twitchEmbed.style.display = 'none';
+amazonLogo.addEventListener('mouseleave', () => {
+    tooltip.style.display = 'none';
+});
+
+// Code pour l'embed Twitch
+const twitchEmbed = document.getElementById('twitch-embed');
+const toggleEmbedButton = document.getElementById('toggle-embed');
+
+toggleEmbedButton.addEventListener('click', () => {
+    if (twitchEmbed.style.display === 'none' || twitchEmbed.style.display === '') {
+        twitchEmbed.style.display = 'block';
+    } else {
+        twitchEmbed.style.display = 'none';
+    }
 });
